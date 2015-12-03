68464c44ad144483fbb1af11a3c74cce3d9e8252
20141105071 <20141105071wyr@mac26.local>
2015-12-03 10:28:36 +0800

    Initial Commit

---
+++ Array cross-border/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Array cross-border
+//
+//  Created by 20141105071 on 15/12/3.
+//  Copyright © 2015年 20141105071. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
